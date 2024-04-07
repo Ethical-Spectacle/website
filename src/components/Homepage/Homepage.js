e4ea955413d30e7c0ce5d4006b796133bd79fbd1
Cristian Zambrano <criszamcham@gmail.com>
2024-04-07 10:59:28 -0700

    Update home page hierarchy

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -6,33 +6,33 @@ import GCNlogo from '../../assets/global_career_network.png';
 
 const Homepage = () => {
   return (
-    <div className="Home">
-      <header className="Home-header">
-        <h1>Ethical Spectacle Research</h1>
-        <p>A melting pot of ethical leaders and techies that write *clean* code...</p>
-        <Link to="/hackathons" className="cta">Join Our Hackathon</Link>
-      </header>
+  <section className="hero-section">
+    <header className="Home-header">
+      <h1>Ethical Spectacle Research</h1>
+      <p>A melting pot of ethical leaders and techies that write *clean* code...</p>
+      <Link to="/hackathons" className="cta">Join Our Hackathon</Link>
+    </header>
 
-      <section id="about" className="About-section">
-        <h1>With Great Power Comes Great Responsibility</h1>
-        <p>The future lies in the hands of developers. Us devs have a responsibility to be careful, without stifling progress. Our mission is to build impactful tech, with the forethought to protect users.</p>
-      </section>
+    <section id="about" className="About-section">
+      <h2>With Great Power Comes Great Responsibility</h2>
+      <p>The future lies in the hands of developers. Us devs have a responsibility to be careful, without stifling progress. Our mission is to build impactful tech, with the forethought to protect users.</p>
+    </section>
 
-      <section id="partners" className="Partners-section">
-        <h1>Partners</h1>
-        <a href="https://azvc.com/" target="_blank" rel="noopener noreferrer">
-          <img className="AZVCLogo" src={AZVClogo} alt="Arizona Venture Capital" style={{ height: '80px'}} />
-        </a> 
-        <a href="https://www.linkedin.com/company/global-career-network/" target="_blank" rel="noopener noreferrer">
-          <img className="GCNLogo" src={GCNlogo} alt="Global Career Network" style={{ height: '50px', marginBottom: '5px'}} />
-        </a>
-      </section>
+    <section id="partners" className="Partners-section">
+      <h2>Partners</h2>
+      <a href="https://azvc.com/" target="_blank" rel="noopener noreferrer">
+        <img className="AZVCLogo" src={AZVClogo} alt="Arizona Venture Capital" style={{ height: '80px'}} />
+      </a> 
+      <a href="https://www.linkedin.com/company/global-career-network/" target="_blank" rel="noopener noreferrer">
+        <img className="GCNLogo" src={GCNlogo} alt="Global Career Network" style={{ height: '50px', marginBottom: '5px'}} />
+      </a>
+    </section>
 
-      <section id="contact" className="Contact-section">
-        <h1>Contact Us</h1>
-        <p>Interested in joining our mission or have questions? Contact our president Maximus Powers at maximuspowersdev@gmail.com</p>
-      </section>
-    </div>
+    <section id="contact" className="Contact-section">
+      <h2>Contact Us</h2>
+      <p>Interested in joining our mission or have questions? Contact our president Maximus Powers at maximuspowersdev@gmail.com</p>
+    </section>
+  </section>
   );
 };
 
